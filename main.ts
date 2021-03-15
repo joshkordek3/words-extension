14e7dffc4d11527696714485c1fffc803caa7a13
joshkordek3 <79750252+joshkordek3@users.noreply.github.com>
2021-03-15 17:21:11 +0800

    removed number extension because it completely broke the code

--- main.ts
+++ main.ts
@@ -98,16 +98,3 @@ function read_from (text: string, _from: string) {
     }
 }
 }
-//% color=#800080 weight=100 icon="\uf1ec block="Advanced Numbers"
-namespace hello {
-//%block
-//% group="Converting Bases"
-export function convert_from_base_to_base_10 (base: number, num: number) {
-    temp_val = 0
-    for (let index = 0; index <= convertToText(num).length - 1; index++) {
-        temp_val += parseFloat(convertToText(num).charAt(convertToText(num).length - (index + 1))) * base ** index
-    }
-    return temp_val
-}
-let temp_val = 0
-}
